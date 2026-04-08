cde53463360b822a524de61fd5fc312ee9f664a3
kappmar1 <kappmar1@students.zhaw.ch>
2026-04-08 11:27:59 +0200

    CHANGE: No SSR, therefore hydration is not needed.

--- solidflow/frontend/src/app/app.config.ts
+++ solidflow/frontend/src/app/app.config.ts
@@ -5,15 +5,10 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideAnimationsAsync(),
     provideRouter(appRoutes),
